6028e9f0ad2d6bb1dd31805221bfe42da8a08eb4
Rusty Burchfield <GICodeWarrior@gmail.com>
2026-04-01 10:01:50 -0700

    Minor binary size optimization

--- includes/wasm/fiw.js
+++ includes/wasm/fiw.js
@@ -1,345 +1,5 @@
 /* @ts-self-types="./fiw.d.ts" */
 
-/**
- * A line of text that has been detected, but not recognized.
- *
- * This contains information about the location of the text, but not the
- * string contents.
- */
-export class DetectedLine {
-    static __wrap(ptr) {
-        ptr = ptr >>> 0;
-        const obj = Object.create(DetectedLine.prototype);
-        obj.__wbg_ptr = ptr;
-        DetectedLineFinalization.register(obj, obj.__wbg_ptr, obj);
-        return obj;
-    }
-    static __unwrap(jsValue) {
-        if (!(jsValue instanceof DetectedLine)) {
-            return 0;
-        }
-        return jsValue.__destroy_into_raw();
-    }
-    __destroy_into_raw() {
-        const ptr = this.__wbg_ptr;
-        this.__wbg_ptr = 0;
-        DetectedLineFinalization.unregister(this);
-        return ptr;
-    }
-    free() {
-        const ptr = this.__destroy_into_raw();
-        wasm.__wbg_detectedline_free(ptr, 0);
-    }
-    /**
-     * @returns {RotatedRect}
-     */
-    rotatedRect() {
-        const ret = wasm.detectedline_rotatedRect(this.__wbg_ptr);
-        return RotatedRect.__wrap(ret);
-    }
-    /**
-     * @returns {RotatedRect[]}
-     */
-    words() {
-        const ret = wasm.detectedline_words(this.__wbg_ptr);
-        var v1 = getArrayJsValueFromWasm0(ret[0], ret[1]).slice();
-        wasm.__wbindgen_free(ret[0], ret[1] * 4, 4);
-        return v1;
-    }
-}
-if (Symbol.dispose) DetectedLine.prototype[Symbol.dispose] = DetectedLine.prototype.free;
-
-/**
- * A pre-processed image that can be passed as input to `OcrEngine.loadImage`.
- */
-export class Image {
-    static __wrap(ptr) {
-        ptr = ptr >>> 0;
-        const obj = Object.create(Image.prototype);
-        obj.__wbg_ptr = ptr;
-        ImageFinalization.register(obj, obj.__wbg_ptr, obj);
-        return obj;
-    }
-    __destroy_into_raw() {
-        const ptr = this.__wbg_ptr;
-        this.__wbg_ptr = 0;
-        ImageFinalization.unregister(this);
-        return ptr;
-    }
-    free() {
-        const ptr = this.__destroy_into_raw();
-        wasm.__wbg_image_free(ptr, 0);
-    }
-    /**
-     * Return the number of channels in the image.
-     * @returns {number}
-     */
-    channels() {
-        const ret = wasm.image_channels(this.__wbg_ptr);
-        return ret >>> 0;
-    }
-    /**
-     * Return the image data in row-major, channels-last order.
-     * @returns {Uint8Array}
-     */
-    data() {
-        const ret = wasm.image_data(this.__wbg_ptr);
-        var v1 = getArrayU8FromWasm0(ret[0], ret[1]).slice();
-        wasm.__wbindgen_free(ret[0], ret[1] * 1, 1);
-        return v1;
-    }
-    /**
-     * Return the height of the image.
-     * @returns {number}
-     */
-    height() {
-        const ret = wasm.image_height(this.__wbg_ptr);
-        return ret >>> 0;
-    }
-    /**
-     * Return the width of the image.
-     * @returns {number}
-     */
-    width() {
-        const ret = wasm.image_width(this.__wbg_ptr);
-        return ret >>> 0;
-    }
-}
-if (Symbol.dispose) Image.prototype[Symbol.dispose] = Image.prototype.free;
-
-/**
- * OcrEngine is the main API for performing OCR in WebAssembly.
- */
-export class OcrEngine {
-    __destroy_into_raw() {
-        const ptr = this.__wbg_ptr;
-        this.__wbg_ptr = 0;
-        OcrEngineFinalization.unregister(this);
-        return ptr;
-    }
-    free() {
-        const ptr = this.__destroy_into_raw();
-        wasm.__wbg_ocrengine_free(ptr, 0);
-    }
-    /**
-     * Detect text in an image.
-     *
-     * Returns a list of lines that were found. These can be passed to
-     * `recognizeText` identify the characters.
-     * @param {Image} image
-     * @returns {DetectedLine[]}
-     */
-    detectText(image) {
-        _assertClass(image, Image);
-        const ret = wasm.ocrengine_detectText(this.__wbg_ptr, image.__wbg_ptr);
-        if (ret[3]) {
-            throw takeFromExternrefTable0(ret[2]);
-        }
-        var v1 = getArrayJsValueFromWasm0(ret[0], ret[1]).slice();
-        wasm.__wbindgen_free(ret[0], ret[1] * 4, 4);
-        return v1;
-    }
-    /**
-     * Detect and recognize text in an image.
-     *
-     * Returns a single string containing all the text found in reading order.
-     * @param {Image} image
-     * @returns {string}
-     */
-    getText(image) {
-        let deferred2_0;
-        let deferred2_1;
-        try {
-            _assertClass(image, Image);
-            const ret = wasm.ocrengine_getText(this.__wbg_ptr, image.__wbg_ptr);
-            var ptr1 = ret[0];
-            var len1 = ret[1];
-            if (ret[3]) {
-                ptr1 = 0; len1 = 0;
-                throw takeFromExternrefTable0(ret[2]);
-            }
-            deferred2_0 = ptr1;
-            deferred2_1 = len1;
-            return getStringFromWasm0(ptr1, len1);
-        } finally {
-            wasm.__wbindgen_free(deferred2_0, deferred2_1, 1);
-        }
-    }
-    /**
-     * Detect and recognize text in an image.
-     *
-     * Returns a list of `TextLine` objects that can be used to query the text
-     * and bounding boxes of each line.
-     * @param {Image} image
-     * @returns {TextLine[]}
-     */
-    getTextLines(image) {
-        _assertClass(image, Image);
-        const ret = wasm.ocrengine_getTextLines(this.__wbg_ptr, image.__wbg_ptr);
-        if (ret[3]) {
-            throw takeFromExternrefTable0(ret[2]);
-        }
-        var v1 = getArrayJsValueFromWasm0(ret[0], ret[1]).slice();
-        wasm.__wbindgen_free(ret[0], ret[1] * 4, 4);
-        return v1;
-    }
-    /**
-     * Prepare an image for analysis by the OCR engine.
-     *
-     * The image is an array of pixels in row-major, channels last order. This
-     * matches the format of the
-     * [ImageData](https://developer.mozilla.org/en-US/docs/Web/API/ImageData)
-     * API. Supported channel combinations are RGB and RGBA. The number of
-     * channels is inferred from the length of `data`.
-     * @param {number} width
-     * @param {number} height
-     * @param {Uint8Array} data
-     * @returns {Image}
-     */
-    loadImage(width, height, data) {
-        const ptr0 = passArray8ToWasm0(data, wasm.__wbindgen_malloc);
-        const len0 = WASM_VECTOR_LEN;
-        const ret = wasm.ocrengine_loadImage(this.__wbg_ptr, width, height, ptr0, len0);
-        if (ret[2]) {
-            throw takeFromExternrefTable0(ret[1]);
-        }
-        return Image.__wrap(ret[0]);
-    }
-    /**
-     * Construct a new `OcrEngine` using the models and other settings given
-     * by `init`.
-     *
-     * To detect text in an image, `init` must have a detection model set.
-     * To recognize text, `init` must have a recognition model set.
-     * @param {OcrEngineInit} init
-     */
-    constructor(init) {
-        _assertClass(init, OcrEngineInit);
-        var ptr0 = init.__destroy_into_raw();
-        const ret = wasm.ocrengine_new(ptr0);
-        if (ret[2]) {
-            throw takeFromExternrefTable0(ret[1]);
-        }
-        this.__wbg_ptr = ret[0] >>> 0;
-        OcrEngineFinalization.register(this, this.__wbg_ptr, this);
-        return this;
-    }
-    /**
-     * Recognize text that was previously detected with `detectText`.
-     *
-     * Returns a list of `TextLine` objects that can be used to query the text
-     * and bounding boxes of each line.
-     * @param {Image} image
-     * @param {DetectedLine[]} lines
-     * @returns {TextLine[]}
-     */
-    recognizeText(image, lines) {
-        _assertClass(image, Image);
-        const ptr0 = passArrayJsValueToWasm0(lines, wasm.__wbindgen_malloc);
-        const len0 = WASM_VECTOR_LEN;
-        const ret = wasm.ocrengine_recognizeText(this.__wbg_ptr, image.__wbg_ptr, ptr0, len0);
-        if (ret[3]) {
-            throw takeFromExternrefTable0(ret[2]);
-        }
-        var v2 = getArrayJsValueFromWasm0(ret[0], ret[1]).slice();
-        wasm.__wbindgen_free(ret[0], ret[1] * 4, 4);
-        return v2;
-    }
-}
-if (Symbol.dispose) OcrEngine.prototype[Symbol.dispose] = OcrEngine.prototype.free;
-
-/**
- * Options for constructing an [OcrEngine].
- */
-export class OcrEngineInit {
-    __destroy_into_raw() {
-        const ptr = this.__wbg_ptr;
-        this.__wbg_ptr = 0;
-        OcrEngineInitFinalization.unregister(this);
-        return ptr;
-    }
-    free() {
-        const ptr = this.__destroy_into_raw();
-        wasm.__wbg_ocrengineinit_free(ptr, 0);
-    }
-    constructor() {
-        const ret = wasm.ocrengineinit_new();
-        this.__wbg_ptr = ret >>> 0;
-        OcrEngineInitFinalization.register(this, this.__wbg_ptr, this);
-        return this;
-    }
-    /**
-     * Load a model for text detection.
-     * @param {Uint8Array} data
-     */
-    setDetectionModel(data) {
-        const ptr0 = passArray8ToWasm0(data, wasm.__wbindgen_malloc);
-        const len0 = WASM_VECTOR_LEN;
-        const ret = wasm.ocrengineinit_setDetectionModel(this.__wbg_ptr, ptr0, len0);
-        if (ret[1]) {
-            throw takeFromExternrefTable0(ret[0]);
-        }
-    }
-    /**
-     * Load a model for text recognition.
-     * @param {Uint8Array} data
-     */
-    setRecognitionModel(data) {
-        const ptr0 = passArray8ToWasm0(data, wasm.__wbindgen_malloc);
-        const len0 = WASM_VECTOR_LEN;
-        const ret = wasm.ocrengineinit_setRecognitionModel(this.__wbg_ptr, ptr0, len0);
-        if (ret[1]) {
-            throw takeFromExternrefTable0(ret[0]);
-        }
-    }
-}
-if (Symbol.dispose) OcrEngineInit.prototype[Symbol.dispose] = OcrEngineInit.prototype.free;
-
-export class RotatedRect {
-    static __wrap(ptr) {
-        ptr = ptr >>> 0;
-        const obj = Object.create(RotatedRect.prototype);
-        obj.__wbg_ptr = ptr;
-        RotatedRectFinalization.register(obj, obj.__wbg_ptr, obj);
-        return obj;
-    }
-    __destroy_into_raw() {
-        const ptr = this.__wbg_ptr;
-        this.__wbg_ptr = 0;
-        RotatedRectFinalization.unregister(this);
-        return ptr;
-    }
-    free() {
-        const ptr = this.__destroy_into_raw();
-        wasm.__wbg_rotatedrect_free(ptr, 0);
-    }
-    /**
-     * Return the coordinates of the axis-aligned bounding rectangle of this
-     * rotated rect.
-     *
-     * The result is a `[left, top, right, bottom]` array of coordinates.
-     * @returns {Float32Array}
-     */
-    boundingRect() {
-        const ret = wasm.rotatedrect_boundingRect(this.__wbg_ptr);
-        var v1 = getArrayF32FromWasm0(ret[0], ret[1]).slice();
-        wasm.__wbindgen_free(ret[0], ret[1] * 4, 4);
-        return v1;
-    }
-    /**
-     * Return an array of the X and Y coordinates of corners of this rectangle,
-     * arranged as `[x0, y0, ... x3, y3]`.
-     * @returns {Float32Array}
-     */
-    corners() {
-        const ret = wasm.rotatedrect_corners(this.__wbg_ptr);
-        var v1 = getArrayF32FromWasm0(ret[0], ret[1]).slice();
-        wasm.__wbindgen_free(ret[0], ret[1] * 4, 4);
-        return v1;
-    }
-}
-if (Symbol.dispose) RotatedRect.prototype[Symbol.dispose] = RotatedRect.prototype.free;
-
 export class ScreenshotProcessor {
     __destroy_into_raw() {
         const ptr = this.__wbg_ptr;
@@ -394,102 +54,6 @@ export class ScreenshotProcessor {
 }
 if (Symbol.dispose) ScreenshotProcessor.prototype[Symbol.dispose] = ScreenshotProcessor.prototype.free;
 
-/**
- * A sequence of `TextWord`s that were recognized, forming a line.
- */
-export class TextLine {
-    static __wrap(ptr) {
-        ptr = ptr >>> 0;
-        const obj = Object.create(TextLine.prototype);
-        obj.__wbg_ptr = ptr;
-        TextLineFinalization.register(obj, obj.__wbg_ptr, obj);
-        return obj;
-    }
-    __destroy_into_raw() {
-        const ptr = this.__wbg_ptr;
-        this.__wbg_ptr = 0;
-        TextLineFinalization.unregister(this);
-        return ptr;
-    }
-    free() {
-        const ptr = this.__destroy_into_raw();
-        wasm.__wbg_textline_free(ptr, 0);
-    }
-    /**
-     * @returns {string}
-     */
-    text() {
-        let deferred1_0;
-        let deferred1_1;
-        try {
-            const ret = wasm.textline_text(this.__wbg_ptr);
-            deferred1_0 = ret[0];
-            deferred1_1 = ret[1];
-            return getStringFromWasm0(ret[0], ret[1]);
-        } finally {
-            wasm.__wbindgen_free(deferred1_0, deferred1_1, 1);
-        }
-    }
-    /**
-     * @returns {TextWord[]}
-     */
-    words() {
-        const ret = wasm.textline_words(this.__wbg_ptr);
-        var v1 = getArrayJsValueFromWasm0(ret[0], ret[1]).slice();
-        wasm.__wbindgen_free(ret[0], ret[1] * 4, 4);
-        return v1;
-    }
-}
-if (Symbol.dispose) TextLine.prototype[Symbol.dispose] = TextLine.prototype.free;
-
-/**
- * Bounding box and text of a word that was recognized.
- */
-export class TextWord {
-    static __wrap(ptr) {
-        ptr = ptr >>> 0;
-        const obj = Object.create(TextWord.prototype);
-        obj.__wbg_ptr = ptr;
-        TextWordFinalization.register(obj, obj.__wbg_ptr, obj);
-        return obj;
-    }
-    __destroy_into_raw() {
-        const ptr = this.__wbg_ptr;
-        this.__wbg_ptr = 0;
-        TextWordFinalization.unregister(this);
-        return ptr;
-    }
-    free() {
-        const ptr = this.__destroy_into_raw();
-        wasm.__wbg_textword_free(ptr, 0);
-    }
-    /**
-     * Return the oriented bounding rectangle containing the characters in
-     * this word.
-     * @returns {RotatedRect}
-     */
-    rotatedRect() {
-        const ret = wasm.textword_rotatedRect(this.__wbg_ptr);
-        return RotatedRect.__wrap(ret);
-    }
-    /**
-     * @returns {string}
-     */
-    text() {
-        let deferred1_0;
-        let deferred1_1;
-        try {
-            const ret = wasm.textword_text(this.__wbg_ptr);
-            deferred1_0 = ret[0];
-            deferred1_1 = ret[1];
-            return getStringFromWasm0(ret[0], ret[1]);
-        } finally {
-            wasm.__wbindgen_free(deferred1_0, deferred1_1, 1);
-        }
-    }
-}
-if (Symbol.dispose) TextWord.prototype[Symbol.dispose] = TextWord.prototype.free;
-
 function __wbg_get_imports() {
     const import0 = {
         __proto__: null,
@@ -512,14 +76,6 @@ function __wbg_get_imports() {
         __wbg___wbindgen_throw_6ddd609b62940d55: function(arg0, arg1) {
             throw new Error(getStringFromWasm0(arg0, arg1));
         },
-        __wbg_detectedline_new: function(arg0) {
-            const ret = DetectedLine.__wrap(arg0);
-            return ret;
-        },
-        __wbg_detectedline_unwrap: function(arg0) {
-            const ret = DetectedLine.__unwrap(arg0);
-            return ret;
-        },
         __wbg_new_49d5571bd3f0c4d4: function() {
             const ret = new Map();
             return ret;
@@ -532,10 +88,6 @@ function __wbg_get_imports() {
             const ret = new Object();
             return ret;
         },
-        __wbg_rotatedrect_new: function(arg0) {
-            const ret = RotatedRect.__wrap(arg0);
-            return ret;
-        },
         __wbg_set_282384002438957f: function(arg0, arg1, arg2) {
             arg0[arg1 >>> 0] = arg2;
         },
@@ -544,14 +96,6 @@ function __wbg_get_imports() {
         },
         __wbg_set_bf7251625df30a02: function(arg0, arg1, arg2) {
             const ret = arg0.set(arg1, arg2);
-            return ret;
-        },
-        __wbg_textline_new: function(arg0) {
-            const ret = TextLine.__wrap(arg0);
-            return ret;
-        },
-        __wbg_textword_new: function(arg0) {
-            const ret = TextWord.__wrap(arg0);
             return ret;
         },
         __wbindgen_cast_0000000000000001: function(arg0) {
@@ -590,62 +134,14 @@ function __wbg_get_imports() {
     };
 }
 
-const DetectedLineFinalization = (typeof FinalizationRegistry === 'undefined')
-    ? { register: () => {}, unregister: () => {} }
-    : new FinalizationRegistry(ptr => wasm.__wbg_detectedline_free(ptr >>> 0, 1));
-const ImageFinalization = (typeof FinalizationRegistry === 'undefined')
-    ? { register: () => {}, unregister: () => {} }
-    : new FinalizationRegistry(ptr => wasm.__wbg_image_free(ptr >>> 0, 1));
-const OcrEngineFinalization = (typeof FinalizationRegistry === 'undefined')
-    ? { register: () => {}, unregister: () => {} }
-    : new FinalizationRegistry(ptr => wasm.__wbg_ocrengine_free(ptr >>> 0, 1));
-const OcrEngineInitFinalization = (typeof FinalizationRegistry === 'undefined')
-    ? { register: () => {}, unregister: () => {} }
-    : new FinalizationRegistry(ptr => wasm.__wbg_ocrengineinit_free(ptr >>> 0, 1));
-const RotatedRectFinalization = (typeof FinalizationRegistry === 'undefined')
-    ? { register: () => {}, unregister: () => {} }
-    : new FinalizationRegistry(ptr => wasm.__wbg_rotatedrect_free(ptr >>> 0, 1));
 const ScreenshotProcessorFinalization = (typeof FinalizationRegistry === 'undefined')
     ? { register: () => {}, unregister: () => {} }
     : new FinalizationRegistry(ptr => wasm.__wbg_screenshotprocessor_free(ptr >>> 0, 1));
-const TextLineFinalization = (typeof FinalizationRegistry === 'undefined')
-    ? { register: () => {}, unregister: () => {} }
-    : new FinalizationRegistry(ptr => wasm.__wbg_textline_free(ptr >>> 0, 1));
-const TextWordFinalization = (typeof FinalizationRegistry === 'undefined')
-    ? { register: () => {}, unregister: () => {} }
-    : new FinalizationRegistry(ptr => wasm.__wbg_textword_free(ptr >>> 0, 1));
 
 function addToExternrefTable0(obj) {
     const idx = wasm.__externref_table_alloc();
     wasm.__wbindgen_externrefs.set(idx, obj);
     return idx;
-}
-
-function _assertClass(instance, klass) {
-    if (!(instance instanceof klass)) {
-        throw new Error(`expected instance of ${klass.name}`);
-    }
-}
-
-function getArrayF32FromWasm0(ptr, len) {
-    ptr = ptr >>> 0;
-    return getFloat32ArrayMemory0().subarray(ptr / 4, ptr / 4 + len);
-}
-
-function getArrayJsValueFromWasm0(ptr, len) {
-    ptr = ptr >>> 0;
-    const mem = getDataViewMemory0();
-    const result = [];
-    for (let i = ptr; i < ptr + 4 * len; i += 4) {
-        result.push(wasm.__wbindgen_externrefs.get(mem.getUint32(i, true)));
-    }
-    wasm.__externref_drop_slice(ptr, len);
-    return result;
-}
-
-function getArrayU8FromWasm0(ptr, len) {
-    ptr = ptr >>> 0;
-    return getUint8ArrayMemory0().subarray(ptr / 1, ptr / 1 + len);
 }
 
 let cachedDataViewMemory0 = null;
@@ -654,14 +150,6 @@ function getDataViewMemory0() {
         cachedDataViewMemory0 = new DataView(wasm.memory.buffer);
     }
     return cachedDataViewMemory0;
-}
-
-let cachedFloat32ArrayMemory0 = null;
-function getFloat32ArrayMemory0() {
-    if (cachedFloat32ArrayMemory0 === null || cachedFloat32ArrayMemory0.byteLength === 0) {
-        cachedFloat32ArrayMemory0 = new Float32Array(wasm.memory.buffer);
-    }
-    return cachedFloat32ArrayMemory0;
 }
 
 function getStringFromWasm0(ptr, len) {
@@ -775,7 +263,6 @@ function __wbg_finalize_init(instance, module) {
     wasm = instance.exports;
     wasmModule = module;
     cachedDataViewMemory0 = null;
-    cachedFloat32ArrayMemory0 = null;
     cachedUint8ArrayMemory0 = null;
     wasm.__wbindgen_start();
     return wasm;
